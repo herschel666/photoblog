e00ce6d345ddcbe8897f0e5660f29a9aa5bf0efe
Emanuel Kluge <emanuel.kluge@jimdo.com>
2017-03-19 14:35:43 +0100

    Provide srcset-attributes to images

--- src/components/image/image.jsx
+++ src/components/image/image.jsx
@@ -1,30 +1,41 @@
 
 import React, { PropTypes } from 'react';
+import slug from 'slug';
 import classnames from 'classnames';
 import { format } from 'date-fns';
 import styles from './image.sass';
 
-const getDetailLinkFromFileName = file =>
-    `/photo${file.replace('.jpg', '')}/`;
+const getDetailLink = (title, src) => {
+    const [, imageId = '1'] = /^\/([^.]+)\.jpg$/i.exec(src);
+    return `/photo/${slug(title.toLowerCase())}-${imageId}/`;
+};
 
-const createImg = (file, isDetail) => {
-    const img = <img src={file} alt="" />;
+const getSizesMap = () => [300, 600, 900].map(i =>
+    `(max-width: ${i}px) ${i}px`).concat(['1200px']).join(',');
+
+const createImg = (src, srcSet, title, isDetail) => {
+    const img = <img
+        src={src}
+        srcSet={srcSet}
+        sizes={getSizesMap()}
+        alt=""
+    />;
     if (isDetail) {
         return img;
     }
     return (
-        <a href={getDetailLinkFromFileName(file)}>
+        <a href={getDetailLink(title, src)}>
             {img}
         </a>
     );
 };
 
 const Image = ({ photo, detail }) => {
-    const { file, meta } = photo;
+    const { srcSet, src, meta } = photo;
     const isDetail = Boolean(detail);
     return (
         <figure className={styles.figure}>
-            {createImg(file, isDetail)}
+            {createImg(src, srcSet, meta.title, isDetail)}
             <figcaption>
                 <time
                     className={classnames(styles.time, {
@@ -41,12 +52,13 @@ const Image = ({ photo, detail }) => {
 
 Image.propTypes = {
     photo: PropTypes.shape({
-        file: PropTypes.string.isRequired,
+        srcSet: PropTypes.string.isRequired,
+        src: PropTypes.string.isRequired,
         meta: PropTypes.shape({
             title: PropTypes.string.isRequired,
-            createdAt: PropTypes.instanceOf(Date),
+            createdAt: PropTypes.instanceOf(Date).isRequired,
         }).isRequired,
-    }),
+    }).isRequired,
     detail: PropTypes.bool,
 };
 
