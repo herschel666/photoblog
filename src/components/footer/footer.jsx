f15e0692648ccf8834aa90afdddb0739d5f19069
Emanuel Kluge <emanuel.kluge@jimdo.com>
2017-04-09 15:03:27 +0200

    Add link to github to footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -5,7 +5,12 @@ import styles from './footer.sass';
 const Footer = () => (
     <footer className={styles.footer}>
         &copy; {(new Date()).getFullYear()} &middot;&nbsp;
-        <a href="/imprint/">Imprint</a>
+        <a href="/imprint/">Imprint</a> &middot;&nbsp;
+        <a
+            target="_blank"
+            rel="noopener, noreferrer"
+            href="https://github.com/herschel666/photoblog/"
+        >View source on GitHub</a>
     </footer>
 );
 
