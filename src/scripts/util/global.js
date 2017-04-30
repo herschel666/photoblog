25d2d3a606832dafd73378494638ebe31f04f426
Emanuel Kluge <kraemerseele@gmx.de>
2017-04-30 12:32:19 +0200

    Add better solutions to store global variables

---
+++ src/scripts/util/global.js
@@ -0,0 +1,31 @@
+
+export const global = window.__ekphotos__;
+
+const keyIsUndefined = key => global[key] === undefined;
+
+const keyIsNull = key => global[key] === null;
+
+export default function setGlobalKey(key, immutable = true) {
+    const helperKeyName = `$$__${key}`;
+    if (keyIsUndefined(helperKeyName)) {
+        Object.defineProperty(global, helperKeyName, {
+            __proto__: null,
+            value: null,
+            writable: true,
+        });
+        Object.defineProperty(global, key, {
+            __proto__: null,
+            enumerable: true,
+            set(value) {
+                if (!keyIsNull(helperKeyName) && immutable) {
+                    return;
+                }
+                global[helperKeyName] = value;
+            },
+            get() {
+                return global[helperKeyName];
+            },
+        });
+    }
+};
+
