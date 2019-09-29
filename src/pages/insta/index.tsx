8f9476bd8d637ace7a428224ab5ba14a984e5215
Emanuel Kluge <kraemerseele@gmx.de>
2019-09-29 12:59:47 +0200

    feat: add open graph image data

--- src/pages/insta/index.tsx
+++ src/pages/insta/index.tsx
@@ -21,10 +21,25 @@ interface Image {
   };
 }
 
+interface OpenGraphNode {
+  file: {
+    localFile: {
+      img: {
+        fixed: {
+          src: string;
+        };
+      };
+    };
+  };
+}
+
 interface Props {
   data: {
     insta: {
       images: Image[];
+    };
+    og: {
+      nodes: OpenGraphNode[];
     };
   };
 }
@@ -46,15 +61,40 @@ export const query = graphql`
         }
       }
     }
+    og: allContentfulImage(limit: 1) {
+      nodes {
+        file {
+          localFile {
+            img: childImageSharp {
+              fixed(width: 1000) {
+                src
+              }
+            }
+          }
+        }
+      }
+    }
   }
 `;
+
+const getOpenGraphImage = (og: OpenGraphNode) => [
+  {
+    property: 'og:image',
+    content: og.file.localFile.img.fixed.src,
+  },
+  { property: 'og:image:width', content: '1000' },
+  { property: 'og:image:height', content: '1000' },
+  { name: 'twitter:image', content: og.file.localFile.img.fixed.src },
+  { name: 'twitter:image:width', content: '1000' },
+  { name: 'twitter:image:height', content: '1000' },
+];
 
 const Insta: React.SFC<Props> = ({ data }) => {
   const Link = useLink();
 
   return (
     <>
-      <Seo title="Insta" />
+      <Seo title="Insta" meta={getOpenGraphImage(data.og.nodes[0])} />
       <Layout>
         <h1>Insta</h1>
         <ImageGrid>
