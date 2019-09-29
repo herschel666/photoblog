7107cb66443346eb47639209c6b004049ba5d7be
Emanuel Kluge <kraemerseele@gmx.de>
2019-09-29 11:01:05 +0200

    test: do not check URLs of prev/next links

--- integration/spec.js
+++ integration/spec.js
@@ -5,17 +5,15 @@ Cypress.Screenshot.defaults({
 const sets = {
   '/winter-hamburg-2018/': {
     title: 'A Winter Night in Ottensen',
-    photos: ['winter-hamburg-20180228-02', 'winter-hamburg-20180228-01'],
     entry: 0,
   },
   '/random-hamburg-2015/': {
     title: 'Random Hamburg 2015',
-    photos: ['hamburg-20151122-05', 'hamburg-20151122-04'],
     entry: 3,
   },
 };
 
-Object.entries(sets).forEach(([pathname, { title, photos, entry }]) =>
+Object.entries(sets).forEach(([pathname, { title, entry }]) =>
   describe(title, () => {
     it('links to the set', () => {
       cy.visit('/');
@@ -29,18 +27,14 @@ Object.entries(sets).forEach(([pathname, { title, photos, entry }]) =>
     });
 
     it('has a navigation between images & back to the set', () => {
-      const [slug1, slug2] = photos;
-
       cy.visit(pathname);
       cy.get('img').should('be.visible');
       cy.get(`a[data-testid="img-link-${entry}"]`)
         .should('be.visible')
         .click();
       cy.get('a[data-testid="prev"]').click();
-      cy.location('pathname').should('eq', `${pathname}${slug1}/`);
       cy.get('a[data-testid="next"]').click();
-      cy.location('pathname').should('eq', `${pathname}${slug2}/`);
-      cy.contains('back').click();
+      cy.contains('back').click({ force: true });
       cy.location('pathname').should('eq', pathname);
     });
   })
