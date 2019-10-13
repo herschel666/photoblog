4910c7ea1db32fcc6e101f164abbf348ee91194a
Emanuel Kluge <kraemerseele@gmx.de>
2019-10-13 09:19:23 +0200

    chore: wait for deployment to be really finished

--- script/deploy.js
+++ script/deploy.js
@@ -1,3 +1,4 @@
+const assert = require('assert');
 const { promisify } = require('util');
 const path = require('path');
 const fs = require('fs');
@@ -7,9 +8,15 @@ const fetch = require('node-fetch');
 const { NETLIFY_ACCESS_TOKEN } = process.env;
 const SRC_DIR = path.join(process.cwd(), 'public');
 const FILE_NAME = path.join(process.cwd(), 'site.zip');
+const API_BASE = 'https://api.netlify.com/api/v1';
+
+assert(NETLIFY_ACCESS_TOKEN, 'The $NETLIFY_ACCESS_TOKEN is defined.');
 
 const stat = promisify(fs.stat);
 const rm = promisify(fs.unlink);
+
+const wait = (delay = 5000) =>
+  new Promise((resolve) => setTimeout(resolve, delay));
 
 const clean = async () => {
   console.log('> Remove exisiting Zip file...');
@@ -38,9 +45,8 @@ const uploadZipFile = async () => {
   console.log('> Upload Zip file...');
   const { size } = await stat(FILE_NAME);
   const body = fs.createReadStream(FILE_NAME);
-
-  await fetch(
-    'https://api.netlify.com/api/v1/sites/ek-photos-cdn.netlify.com/deploys',
+  const response = await fetch(
+    `${API_BASE}/sites/ek-photos-cdn.netlify.com/deploys`,
     {
       method: 'POST',
       headers: {
@@ -51,11 +57,45 @@ const uploadZipFile = async () => {
       body,
     }
   );
+
+  return response.json();
 };
 
-const finish = () => console.log('> Successfully deployed site.');
+const getDeployState = async (deployId) => {
+  const response = await fetch(`${API_BASE}/deploys/${deployId}`, {
+    headers: {
+      authorization: `Bearer ${NETLIFY_ACCESS_TOKEN}`,
+    },
+  });
+  const { state } = await response.json();
+  return state;
+};
 
-[clean, writeZipFile, uploadZipFile, finish].reduce(
+const isPending = (state) => state !== 'ready' && state !== 'error';
+
+const checkForReady = async ({ id: deployId }) => {
+  let state = 'unknown';
+
+  process.stdout.write('> Check for deployment state...');
+  while (isPending(state)) {
+    await wait();
+    state = await getDeployState(deployId);
+    process.stdout.write('.');
+  }
+  process.stdout.write('\n');
+
+  return state;
+};
+
+const finish = (state) => {
+  if (state === 'error') {
+    console.error('> Failed deploying site.');
+    process.exit(1);
+  }
+  console.log('> Successfully deployed site.');
+};
+
+[clean, writeZipFile, uploadZipFile, checkForReady, finish].reduce(
   (promise, fn) => promise.then(fn, console.error),
   Promise.resolve()
 );
