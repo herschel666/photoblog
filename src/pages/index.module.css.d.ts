933baf4dd14c18a7079041ff7f6996d016a14ca7
Emanuel Kluge <kraemerseele@gmx.de>
2020-07-26 11:42:40 +0200

    feat: overhaul sidebar design

--- src/pages/index.module.css.d.ts
+++ src/pages/index.module.css.d.ts
@@ -1,5 +1,7 @@
 export const intro: string;
+export const aside: string;
 export const image: string;
 export const headline: string;
 export const list: string;
 export const imageLink: string;
+export const moreLink: string;
