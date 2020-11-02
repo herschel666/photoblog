3ec5f45528ff6e9eb67042b6f013d566c0a7f73c
Emanuel Kluge <kraemerseele@gmx.de>
2020-11-02 19:45:46 +0100

    feat: add tracking

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,24 @@
+/* eslint-env browser */
 const { config } = require('@fortawesome/fontawesome-svg-core');
 require('@fortawesome/fontawesome-svg-core/styles.css');
 
 config.autoAddCss = false;
+
+const trackPageView = () => {
+  if (
+    process.env.NODE_ENV !== 'development' &&
+    typeof window.track === 'function'
+  ) {
+    window.track(window);
+  }
+};
+
+exports.onClientEntry = () => {
+  trackPageView();
+};
+
+exports.onRouteUpdate = ({ prevLocation }) => {
+  if (prevLocation !== null) {
+    trackPageView();
+  }
+};
