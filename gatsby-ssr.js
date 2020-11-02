71b7107bf226710ad6095c68b3516fc56efec7f7
Emanuel Kluge <kraemerseele@gmx.de>
2020-11-02 20:27:01 +0100

    fix: pass correct referrer string into tracking

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -6,7 +6,7 @@ function track(win) {
     'b5e310247689f41ca09583015c02338479d8023a9ab864e8b216daea9300305b_1280a60ce28d3769d28a21b0e647be11';
   const { pathname, search } = win.location;
   const resource = encodeURIComponent(pathname + search);
-  const { referrer } = encodeURIComponent(win.document.referrer);
+  const referrer = encodeURIComponent(win.document.referrer);
   const src = `${pixel}?id=${trackingId}&resource=${resource}&referrer=${referrer}`;
   Object.assign(new win.Image(), { src });
 }
