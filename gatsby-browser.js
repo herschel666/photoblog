b479b6c95e3a1314f388f27e8cb4e53adccc63a2
Emanuel Kluge <kraemerseele@gmx.de>
2020-11-03 18:58:48 +0100

    fix: prevent tracking on localhost

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,21 +4,12 @@ require('@fortawesome/fontawesome-svg-core/styles.css');
 
 config.autoAddCss = false;
 
-const trackPageView = () => {
-  if (
-    process.env.NODE_ENV !== 'development' &&
-    typeof window.track === 'function'
-  ) {
-    window.track(window);
-  }
-};
-
 exports.onClientEntry = () => {
-  trackPageView();
+  window.track(window);
 };
 
 exports.onRouteUpdate = ({ prevLocation }) => {
   if (prevLocation !== null) {
-    trackPageView();
+    window.track(window);
   }
 };
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,7 +4,10 @@ function track(win) {
   const pixel = 'https://analytics.e5l.de/cctv.gif';
   const trackingId =
     'b5e310247689f41ca09583015c02338479d8023a9ab864e8b216daea9300305b_1280a60ce28d3769d28a21b0e647be11';
-  const { pathname, search } = win.location;
+  const { pathname, search, hostname } = win.location;
+  if (hostname === 'localhost') {
+    return;
+  }
   const resource = encodeURIComponent(pathname + search);
   const referrer = encodeURIComponent(win.document.referrer);
   const src = `${pixel}?id=${trackingId}&resource=${resource}&referrer=${referrer}`;
